6845f65dbd5b27e53d7fce2c6420f81e7bef5c9c
Matheus Freitas <matheus.freitas@sonatalab.com>
2019-10-16 16:56:15 -0300

    working program

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
 var userInputArray = []
+var divTracker = document.getElementById('informationBox')
 
-function adicionar() {
-  let userInputValue = Number(document.getElementById('userInput').value)
-  userInputArray.push(userInputValue)
-  if (userInputValue > 100 || userInputValue < 1 || userInputValue.length == 0) {
-    alert('Há algo de errado, reveja os dados')
+
+function isNumber(n) {
+  //numero nao atende minhas condicoes
+  if(n < 1 || n > 100) {
+    return false
+  }
+  //Numero atende
+  else {
+    return true
+  }
+}
+
+function inList(number, lista){
+  if (lista.indexOf(number) != -1){
+    return true
+  }
+  else {
+    return false
+  }
+
+}
+
+function addUserInput() {
+  let userInput = document.getElementById('userInput')
+  let userInputNumber = Number(userInput.value)
+  
+  if (isNumber(userInputNumber) && !inList(userInputNumber, userInputArray)) {
+     userInputArray.push(userInputNumber)
+    // userInputArray.indexOf(userInputNumber) >= 0
+    let newOption = document.createElement('option')
+    document.getElementById('selectContainer').appendChild(newOption)
+    newOption.innerText = `Número ${userInputNumber} adicionado` 
+    userInput.value = ''
+    userInput.focus()
+    divTracker.innerHTML = ''
   }
   
   else {
-    var newOption = document.createElement('option')
-    document.getElementById('selectContainer').appendChild(newOption)
-    newOption.innerText = userInputValue
-
+    alert('Há algo de errado, reveja os dados')
   }
-  
 }
 
-function finalizar() {
-  // console.log(userInputArray)
-  var newParagraph = document.createElement('p')
-  document.getElementById('informationBox').appendChild(newParagraph)
-
+function userResults() {
+  if (userInputArray.length == 0){
+    alert('Adicione os números primeiro')
+  }
+  else{
     
-
-    newParagraph.innerHTML = `Há ${userInputArray.length} elementos cadastrados <br/>`
-    // console.log(`Há ${userInputArray.length} cadastrados`)
-    newParagraph.innerHTML = newParagraph.innerHTML + `O maior valor é ${Math.max(...userInputArray)}<br/>`
-    // console.log('O maior valor é', Math.max(...userInputArray))
-    newParagraph.innerHTML = newParagraph.innerHTML + `O menor valor é ${Math.min(...userInputArray)}<br/>`
-    // console.log('O menor valor é', Math.min(...userInputArray))
+    let newParagraph = document.createElement('p')
+    document.getElementById('informationBox').appendChild(newParagraph)
+    
+    divTracker.innerHTML = `Há ${userInputArray.length} elementos cadastrados <br/>`
+    divTracker.innerHTML += `O maior valor é ${Math.max(...userInputArray)}<br/>`
+    divTracker.innerHTML += `O menor valor é ${Math.min(...userInputArray)}<br/>`
     var soma = 0
-      for(let a = 0; a < userInputArray.length; a++) {
-        var soma = soma + userInputArray[a]
+      for(let arrayKey = 0; arrayKey < userInputArray.length; arrayKey++) {
+        var soma = soma + userInputArray[arrayKey]
       }
-    newParagraph.innerHTML = newParagraph.innerHTML + `A soma de todos os valores é ${soma}<br/>`
-    // console.log('a soma de todos os valores é de', soma)
-    newParagraph.innerHTML = newParagraph.innerHTML + `A média de todos os valores é ${soma/userInputArray.length}`
-    // console.log('a média dos valores é de', soma/userInputArray.length)
+    divTracker.innerHTML += `A soma de todos os valores é ${soma}<br/>`
+    divTracker.innerHTML += `A média de todos os valores é ${soma/userInputArray.length}`    
     
+  }
 }
